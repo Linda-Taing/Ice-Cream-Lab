7013f63f681ab9eaf69f9cfc8c8f241ec50f5907
Linda Taing <lindatdoan@me.com>
2023-01-26 00:51:27 -0700

    Started over and remembering the sequence of event

--- apps.js
+++ apps.js
@@ -1,26 +1,22 @@
-let shop = [
-    {
+let shop = [{
     name: 'Cookie Dough',
     image: 'https://celebratingsweets.com/wp-content/uploads/2014/04/Cookie-Dough-Ice-Cream-1-5.jpg',
     price: 1
-    
-}, 
-{
+}, {
     name: 'Vanilla',
     image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ultimate-vanilla-ice-cream-1628511695.jpg',
     price: 1
-}, 
-{
+}, {
     name: 'Strawberry',
     image: 'https://www.realfoodwithjessica.com/wp-content/uploads/2017/07/paleostrawberryicecream2.jpg',
     price: 2
 },
+
 {
     name: 'Sprinkles',
     image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Sprinkles2.jpg',
     price: 1
-},
-{
+}, {
     name: 'Chocolate Chips',
     image: 'https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/chocolate-chips.jpg?quality=82&strip=1&resize=640%2C360',
     price: 2
@@ -29,12 +25,13 @@ let shop = [
     name: 'Waffle Cone',
     image: 'https://m.media-amazon.com/images/I/71VNjBMakfL._SL1500_.jpg',
     price: 2
-}, 
-{
+}, {
     name: 'Waffle Bowl',
     image: 'http://images.wbmason.com/350/L_JOY66050.jpg',
     price: 4
 }]
 
-
-
+function addItemToCart(name){
+let itemToAdd = shop.find(p=> p.name == name)
+console.log(itemToAdd)
+}
